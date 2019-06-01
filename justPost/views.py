5639454163ca4b3f9fed8b61a377cccc8d841ea5
kuropyatnik <kuropyatnik27@gmail.com>
2019-06-01 21:52:37 +0300

    View for author

--- justPost/views.py
+++ justPost/views.py
@@ -52,7 +52,7 @@ def indexView(request):
             form = PostForm()
 
             posts = paginateRecords(request,
-                                    Posts.objects.filter(author_id__exact=request.user.id).order_by('-pub_date'), 2)
+                                    Posts.objects.filter(author_id__exact=request.user.id).order_by('-pub_date'))
 
             return render(request, 'home.html', {'form': form, 'posts': posts})
         elif request.method == 'POST':
@@ -74,6 +74,7 @@ def allPostsView(request):
     if request.user.is_authenticated:
         if request.method == 'GET':
             posts = Posts.objects.filter().order_by('-pub_date')
+            posts = paginateRecords(request, posts)
             return render(request, 'posts.html', {'posts': posts})
     else:
         return redirect('login')
@@ -82,12 +83,15 @@ def authorPostsView(request,stub):
     if request.user.is_authenticated:
         if request.method == 'GET':
             posts = Posts.objects.filter(author__user__username__exact=stub).order_by('-pub_date')
-            return render(request, 'posts.html', {'posts': posts})
+            author = User.objects.filter(username__exact=stub).first()
+            posts = paginateRecords(request, posts)
+            return render(request, 'author.html',
+                          {'posts': posts, 'author_name': author.username, 'about_author': author.profile.about_me})
     else:
         return redirect('login')
 
 
-def paginateRecords(request, records, num_records):
+def paginateRecords(request, records, num_records=2):
     page = request.GET.get('page', 1)
     paginator = Paginator(records, num_records)
     try:
